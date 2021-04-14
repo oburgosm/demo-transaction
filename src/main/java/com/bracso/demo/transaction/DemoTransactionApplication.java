871165e8f753dd6a12372a5ad0cbdf0e5d8ee5dd
Oscar Burgos Martin <oscarbma@ext.inditex.com>
2021-04-14 17:08:16 +0200

    Unnecessary annotation removed

--- src/main/java/com/bracso/demo/transaction/DemoTransactionApplication.java
+++ src/main/java/com/bracso/demo/transaction/DemoTransactionApplication.java
@@ -2,13 +2,8 @@ package com.bracso.demo.transaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
-
-import com.bracso.demo.transaction.config.DataSourceConfiguration;
-import com.bracso.demo.transaction.config.EM1Configuration;
 
 @SpringBootApplication
-@Import({DataSourceConfiguration.class, EM1Configuration.class})
 public class DemoTransactionApplication {
 
     public static void main(String[] args) {
